1c6d22954e2fe0e76272f72dbab99a80058f5453
argentum47 <princeofpersiaa3.ag@gmail.com>
2016-05-28 19:24:07 +0530

    initial commit

---
+++ app/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('driverApp', [
+  'driverApp.filters',
+  'driverApp.factories',
+  'driverApp.controllers',
+  'driverApp.components',
+  'driverApp.directives'
+]);
---
+++ app/init.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('driverApp.factories', [])
+angular.module('driverApp.controllers', [])
+angular.module('driverApp.filters', [])
+angular.module('driverApp.components', [])
+angular.module('driverApp.directives', [])
